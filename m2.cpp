aaab44253434d50c1a5432ddd5d46c73f8426590
Jay Krell <jaykrell@microsoft.com>
2018-12-18 06:14:08 -0800

    15 work in progress

--- m2.cpp
+++ m2.cpp
@@ -681,79 +681,6 @@ struct metadata_stream_header_t // see mono verify_metadata_header
     char   Name [32]; // multiple of 4, null terminated, max 32
 };
 
-struct metadata_module_t // 0
-{
-    uint16 Generation; // reserved, 0
-    uint32 Name; // index into string heap
-    uint32 Mvid; // index into guid heap
-    uint32 EncId; // reserved, Guid, 0
-    uint32 EncBaseId; // reserved, Guid, 0
-};
-
-struct metadata_typedef_t // 2
-{
-    enum class flags_t : uint32
-    {
-        // Use this mask to retrieve the type visibility information.
-        VisibilityMask        =   0x00000007,
-        NotPublic             =   0x00000000,     // Class is not public scope.
-        Public                =   0x00000001,     // Class is public scope.
-        NestedPublic          =   0x00000002,     // Class is nested with public visibility.
-        NestedPrivate         =   0x00000003,     // Class is nested with private visibility.
-        NestedFamily          =   0x00000004,     // Class is nested with family visibility.
-        NestedAssembly        =   0x00000005,     // Class is nested with assembly visibility.
-        NestedFamANDAssem     =   0x00000006,     // Class is nested with family and assembly visibility.
-        NestedFamORAssem      =   0x00000007,     // Class is nested with family or assembly visibility.
-
-        // Use this mask to retrieve class layout information
-        LayoutMask            =   0x00000018,
-        AutoLayout            =   0x00000000,     // Class fields are auto-laid out
-        SequentialLayout      =   0x00000008,     // Class fields are laid out sequentially
-        ExplicitLayout        =   0x00000010,     // Layout is supplied explicitly
-        // end layout mask
-
-        // Use this mask to retrieve class semantics information.
-        ClassSemanticsMask    =   0x00000060,
-        Class                 =   0x00000000,     // Type is a class.
-        Interface             =   0x00000020,     // Type is an interface.
-        // end semantics mask
-
-        // Special semantics in addition to class semantics.
-        Abstract              =   0x00000080,     // Class is abstract
-        Sealed                =   0x00000100,     // Class is concrete and may not be extended
-        SpecialName           =   0x00000400,     // Class name is special. Name describes how.
-
-        // Implementation attributes.
-        Import                =   0x00001000,     // Class / interface is imported
-        Serializable          =   0x00002000,     // The class is Serializable.
-
-        // Use StringFormatMask to retrieve string information for native interop
-        StringFormatMask      =   0x00030000,
-        AnsiClass             =   0x00000000,     // LPTSTR is interpreted as ANSI in this class
-        UnicodeClass          =   0x00010000,     // LPTSTR is interpreted as UNICODE
-        AutoClass             =   0x00020000,     // LPTSTR is interpreted automatically
-        CustomFormatClass     =   0x00030000,     // A non-standard encoding specified by CustomFormatMask
-        CustomFormatMask      =   0x00C00000,     // Use this mask to retrieve non-standard encoding information for native interop. The meaning of the values of these 2 bits is unspecified.
-
-        // end string format mask
-
-        BeforeFieldInit       =   0x00100000,     // Initialize the class any time before first static field access.
-        Forwarder             =   0x00200000,     // This ExportedType is a type forwarder.
-
-        // Flags reserved for runtime use.
-        ReservedMask          =   0x00040800,
-        RTSpecialName         =   0x00000800,     // Runtime should check name encoding.
-        HasSecurity           =   0x00040000,     // Class has security associate with it.
-    };
-
-    flags_t flags;
-    uint32 TypeName; // string
-    uint32 TypeNamespace; // string
-    uint32 Extends; // TypeDef or TypeRef or TypeSpec
-    uint32 FieldList; // index into Field table, either to last row or next start
-    uint32 MethodList; // similar to previous
-};
-
 struct metadata_fieldtable_t // 4
 {
     enum class flags_t : uint16
@@ -1867,9 +1794,12 @@ const metadata_field_type_t metadata_field_type_uint8 = {"uint8", 1};
 const metadata_field_type_t metadata_field_type_uint16 = {"uint16", 2};
 const metadata_field_type_t metadata_field_type_uint32 = {"uint32", 4};
 const metadata_field_type_t metadata_field_type_uint64 = {"uint64", 8};
-const metadata_field_type_t metadata_field_type_resolutionscope = {"resolutionscope"};
+const metadata_field_type_t metadata_field_type_ResolutionScope = {"ResolutionScope"};
 const metadata_field_type_t metadata_field_type_string = {"string"};
 const metadata_field_type_t metadata_field_type_guid = {"guid"};
+const metadata_field_type_t metadata_field_type_TypeDefOrRef = {"TypeDefOrRef"};
+const metadata_field_type_t metadata_field_type_FieldList = {"FieldList"}; // TODO
+const metadata_field_type_t metadata_field_type_MethodList = {"MethodList"}; // TODO
 
 struct metadata_field_t
 {
@@ -1888,24 +1818,106 @@ struct metadata_table_schema_t
 #define CountOf(x) (sizeof (x) / sizeof ((x)[0])) // TODO
 
 // 0
+struct metadata_module_t // 0
+{
+    uint16 Generation; // reserved, 0
+    uint32 Name; // index into string heap
+    uint32 Mvid; // index into guid heap
+    uint32 EncId; // reserved, Guid, 0
+    uint32 EncBaseId; // reserved, Guid, 0
+};
 const metadata_field_t metata_row_schema_module_fields [ ] =
 {
-    { "Generation", metadata_field_type_uint16 },
+    { "Generation", metadata_field_type_uint16 }, // ignore
     { "Name", metadata_field_type_string },
     { "Mvid", metadata_field_type_guid },
-    { "EncId", metadata_field_type_guid },
-    { "EncBaseId", metadata_field_type_guid },
+    { "EncId", metadata_field_type_guid }, // ignore
+    { "EncBaseId", metadata_field_type_guid }, // ignore
 };
 metadata_table_schema_t metata_row_schema_module = { "module", CountOf (metata_row_schema_module_fields), metata_row_schema_module_fields };
 
 // 1
 const metadata_field_t metata_row_schema_typeref_fields [ ] =
 {
-    { "ResolutionScope", metadata_field_type_resolutionscope },
+    { "ResolutionScope", metadata_field_type_ResolutionScope },
     { "TypeName", metadata_field_type_string },
     { "TypeNamespace", metadata_field_type_string },
 };
 metadata_table_schema_t metata_row_schema_typeref = { "typeref", CountOf (metata_row_schema_typeref_fields), metata_row_schema_typeref_fields };
+
+struct metadata_typedef_t // 2
+{
+    enum class flags_t : uint32
+    {
+        // Use this mask to retrieve the type visibility information.
+        VisibilityMask        =   0x00000007,
+        NotPublic             =   0x00000000,     // Class is not public scope.
+        Public                =   0x00000001,     // Class is public scope.
+        NestedPublic          =   0x00000002,     // Class is nested with public visibility.
+        NestedPrivate         =   0x00000003,     // Class is nested with private visibility.
+        NestedFamily          =   0x00000004,     // Class is nested with family visibility.
+        NestedAssembly        =   0x00000005,     // Class is nested with assembly visibility.
+        NestedFamANDAssem     =   0x00000006,     // Class is nested with family and assembly visibility.
+        NestedFamORAssem      =   0x00000007,     // Class is nested with family or assembly visibility.
+
+        // Use this mask to retrieve class layout information
+        LayoutMask            =   0x00000018,
+        AutoLayout            =   0x00000000,     // Class fields are auto-laid out
+        SequentialLayout      =   0x00000008,     // Class fields are laid out sequentially
+        ExplicitLayout        =   0x00000010,     // Layout is supplied explicitly
+        // end layout mask
+
+        // Use this mask to retrieve class semantics information.
+        ClassSemanticsMask    =   0x00000060,
+        Class                 =   0x00000000,     // Type is a class.
+        Interface             =   0x00000020,     // Type is an interface.
+        // end semantics mask
+
+        // Special semantics in addition to class semantics.
+        Abstract              =   0x00000080,     // Class is abstract
+        Sealed                =   0x00000100,     // Class is concrete and may not be extended
+        SpecialName           =   0x00000400,     // Class name is special. Name describes how.
+
+        // Implementation attributes.
+        Import                =   0x00001000,     // Class / interface is imported
+        Serializable          =   0x00002000,     // The class is Serializable.
+
+        // Use StringFormatMask to retrieve string information for native interop
+        StringFormatMask      =   0x00030000,
+        AnsiClass             =   0x00000000,     // LPTSTR is interpreted as ANSI in this class
+        UnicodeClass          =   0x00010000,     // LPTSTR is interpreted as UNICODE
+        AutoClass             =   0x00020000,     // LPTSTR is interpreted automatically
+        CustomFormatClass     =   0x00030000,     // A non-standard encoding specified by CustomFormatMask
+        CustomFormatMask      =   0x00C00000,     // Use this mask to retrieve non-standard encoding information for native interop. The meaning of the values of these 2 bits is unspecified.
+
+        // end string format mask
+
+        BeforeFieldInit       =   0x00100000,     // Initialize the class any time before first static field access.
+        Forwarder             =   0x00200000,     // This ExportedType is a type forwarder.
+
+        // Flags reserved for runtime use.
+        ReservedMask          =   0x00040800,
+        RTSpecialName         =   0x00000800,     // Runtime should check name encoding.
+        HasSecurity           =   0x00040000,     // Class has security associate with it.
+    };
+
+    flags_t flags;
+    uint32 TypeName; // string
+    uint32 TypeNamespace; // string
+    uint32 Extends; // TypeDef or TypeRef or TypeSpec
+    uint32 FieldList; // index into Field table, either to last row or next start
+    uint32 MethodList; // similar to previous
+};
+const metadata_field_t metata_row_schema_typedef_fields [ ] =
+{
+    { "Flags", metadata_field_type_uint32 },
+    { "TypeName", metadata_field_type_string },
+    { "TypeNamespace", metadata_field_type_string },
+    { "Extends", metadata_field_type_TypeDefOrRef },
+    { "FieldList", metadata_field_type_FieldList },
+    { "MethodList", metadata_field_type_MethodList },
+};
+metadata_table_schema_t metata_row_schema_typedef = { "typedef", CountOf (metata_row_schema_typedef_fields), metata_row_schema_typedef_fields };
 
 struct metadata_table_t
 {
